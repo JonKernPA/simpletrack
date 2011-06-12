d7d2153bfa8e482c49fe630dbb5b64310baac856
Jon Kern <jonkern@comcast.net>
2011-06-12 19:49:44 -0400

    config

--- website.rb
+++ website.rb
@@ -1,6 +1,6 @@
-# require 'rubygems'
+require 'rubygems'
 require 'sinatra'
-require 'haml'
+# require 'haml'
 # set :views, File.dirname(__FILE__) + "/views"
 # set :public, File.dirname(__FILE__) + "/public"
 # set :run, true
