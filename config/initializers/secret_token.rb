7f92f74cdafcef41e2a8f9fea0d64793971dff81
Jon Kern <jonkern@comcast.net>
2011-06-12 19:30:23 -0400

    dropping rails bits

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Simpletrack::Application.config.secret_token = 'ee2cbbe9220d83c0ad413575170389e08f85c22162dfd685371ed72ec935a7bb02bf31418a83b7b7d8652c9af876372e7b3cb7877998ec8f4503ee1d95c3f024'
--- website.rb
+++ website.rb
@@ -1,5 +1,8 @@
-require 'rubygems'
+# require 'rubygems'
 require 'sinatra'
+# set :views, File.dirname(__FILE__) + "/views"
+# set :public, File.dirname(__FILE__) + "/public"
+set :run, true
 
 get '/' do
   "Welcome to SimpleTrack!"
