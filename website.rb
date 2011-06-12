f464e9e56a13415c49381a1cf911417c1a4f8bf8
Jon Kern <jonkern@comcast.net>
2011-06-12 19:53:13 -0400

    config

--- website.rb
+++ website.rb
@@ -1,9 +1,4 @@
-require 'rubygems'
 require 'sinatra'
-# require 'haml'
-# set :views, File.dirname(__FILE__) + "/views"
-# set :public, File.dirname(__FILE__) + "/public"
-# set :run, true
 
 get '/' do
   "Welcome to SimpleTrack!"
